f0047d671b6a9c4f4a2aae41f26eed5fd0e3344f
FilipK7 <filipkisgeci7@gmail.com>
2023-08-18 22:56:23 +0200

    Provera commit-a

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,5 @@
 const dbConfig = require("../config/db.config.js");
-
+//proba
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
